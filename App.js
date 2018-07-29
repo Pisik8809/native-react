be31e7a3f647862509ea6ca7a032508f5a8272ca
Maxim Shpilka <pisik8809@gmail.com>
2018-07-29 19:40:19 +0300

    second-1

--- App.js
+++ App.js
@@ -1,47 +1,46 @@
 import React from 'react';
-import { Button, View, Text, SectionList } from 'react-native';
+import { Button, View, Text, FlatList, Image, TouchableHighlight } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 import Fetch from 'react-native-fetch';
 import Unsplash from 'unsplash-js/native';
 
-const unsplash = new Unsplash({
-  applicationId: "{32638}",
-  secret: "{46464d17b8a5168597f97ee917077b16b7afd52beef8efd9851e9177dc461b1e}",
-  callbackUrl: "{urn:ietf:wg:oauth:2.0:oob}"
-});
-console.log(unsplash);
-  const listData = {
-    data: []
+/*fetchData = async () => {
+    const response = await fetch('https://unsplash.com/collections/2421912/functional-forum');
+    const json = response.json();
+    this.setState({data: json.results});
   };
-  
+console.log(fetchData());*/
+
 class HomeScreen extends React.Component {
+ cloud = {
+    data: [{gender:"male",name:{title:"mr", first:"reggie",last:"weekers"}, img: "https://randomuser.me/api/portraits/med/men/94.jpg"},
+           {gender:"female",name:{title:"mrs",first:"frances",last:"simpson"}, img: "https://randomuser.me/api/portraits/med/women/70.jpg"}]
+};
+  /*componentDidMount() = {
+    //this.fetchData();
+    console.log('11111');
+  };
+
+  fetchData = async () => {
+    const response = await fetch('https://randomuser.me/api?results=10');
+    const json = response.json();
+    this.setState({data: json.results});
+  };*/
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red'}}>
-        <Text style={{color: 'green', fontSize: 30}}>Home Screen</Text>
-        <Fetch
-          url="https://randomuser.me/api?results=10"
-          retries={3}
-          timeout={3000}
-          onResponse={async (res) => {
-            const json = await res.json();
-            //listData.data[0] = json.id;
-            //console.log(json);
-          }}
-          onError={console.error}
-        />
-        <SectionList
-            renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
-            renderSectionHeader={({section: {title}}) => (
-              <Text style={{fontWeight: 'bold'}}>{title}</Text>
-            )}
-            sections={[
-              {title: 'Title1', data: ['item111', 'item2']},
-              {title: 'Title2', data: ['item3', 'item4']},
-              {title: 'Title3', data: ['item5', 'item6']},
-            ]}
-            keyExtractor={(item, index) => item + index}
-          />
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', flexWrap: 'wrap', 
+      alignItems: 'center', backgroundColor: '#333', alignContent: 'center'}}>
+        <Text style={{color: 'green', fontSize: 30}}>First screen</Text>       
+          <FlatList style={{flex: 1, justifyContent: 'center', alignItems: 'center', 
+          backgroundColor: 'yellow', width: 400, textAlign: 'center'}}
+          data={this.cloud.data}
+          keyExtractor={(x, i) => i}
+          renderItem={({ item }) =>
+          <Text>
+          {`${item.name.first} ${item.name.last}`}
+          <Image source={{uri: item.img}} style={{width: 40, height: 40}}/>
+          </Text>}
+          /> 
         <Button 
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
@@ -55,7 +54,7 @@ class DetailsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
+        <Text>Second Screen</Text>
       </View>
     );
   }
@@ -78,6 +77,16 @@ export default class App extends React.Component {
 }
 
 /*
+const unsplash = new Unsplash({
+  applicationId: "{32638}",
+  secret: "{46464d17b8a5168597f97ee917077b16b7afd52beef8efd9851e9177dc461b1e}",
+  callbackUrl: "{urn:ietf:wg:oauth:2.0:oob}"
+});
+
+console.log(unsplash.photo.getRandomPhoto());
+  const listData = {
+    data: []
+  };
 <Fetch
           url="https://jsonplaceholder.typicode.com/posts/1"
           retries={3}
@@ -112,7 +121,18 @@ FlatList
           {`${item.name.first} ${item.name.last}`}
           </Text>}
           />     
-
+<SectionList
+            renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
+            renderSectionHeader={({section: {title}}) => (
+              <Text style={{fontWeight: 'bold'}}>{title}</Text>
+            )}
+            sections={[
+              {title: 'Title1', data: ['item111', 'item2']},
+              {title: 'Title2', data: ['item3', 'item4']},
+              {title: 'Title3', data: ['item5', 'item6']},
+            ]}
+            keyExtractor={(item, index) => item + index}
+          />
 
  
 
